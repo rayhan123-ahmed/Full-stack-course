e73ada4abdb83ec614e00fac31151a8145f38c77
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-03 19:31:09 +0600

    Non Primative

---
+++ JsFoundation/part2/Nonprimatives.js
@@ -0,0 +1,14 @@
+const userName = {
+    firstName : 'rayhan',
+    isLoggedIn :true
+}
+userName.firstName = 'rifat'
+userName.lastName = 'ahmed'
+
+// console.log(userName);
+
+// array
+
+const user = ['rayhan',true]
+
+// console.log(user[0]);
