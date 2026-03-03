8c8db9023988bcdf41d69bc0b237a7e87661daef
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-03 18:59:55 +0600

    primative

---
+++ JsFoundation/part2/primatives.js
@@ -0,0 +1,34 @@
+// Number
+
+let balance = 120
+let anotherBalance = new Number(120)
+
+// console.log(balance);
+// console.log(anotherBalance.valueOf() );
+// console.log(typeof balance);
+// console.log(typeof anotherBalance);
+
+// boolean 
+let isActive = true
+let actived = new Boolean(false)
+
+// Null and undifined
+let firstname;
+let lastname = null
+
+// console.log(firstname);
+// console.log(lastname);
+
+// String
+
+let firstName = 'rayhan ahmed'
+let lastName = " rifat"
+let fullName = firstName + lastName
+// console.log(fullName);
+let greetMassage = `hey i am ${fullName}`
+// console.log(greetMassage);
+let demon = `value is  ${2 * 5}`
+console.log(demon);
+
+
+
