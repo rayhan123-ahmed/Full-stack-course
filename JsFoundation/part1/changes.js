3ada8f04838e304fae56a50a4c9293e7f2e40c6a
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-03 14:19:43 +0600

    Printing,datatypes,changes

---
+++ JsFoundation/part1/changes.js
@@ -0,0 +1,11 @@
+let superMan = 'batman'
+
+ superMan= 'spiderman'
+ console.log(superMan);
+
+ const games = 'pubg'
+
+//  games= 'ludu'
+ console.log(games);
+ 
+ 
---
+++ JsFoundation/part1/datatype.js
@@ -0,0 +1,14 @@
+/*
+data type
+
+1.string
+2.number
+3.boolean
+4.bigint
+
+5.undefined
+6.null
+
+7.object
+8.symbol
+*/
