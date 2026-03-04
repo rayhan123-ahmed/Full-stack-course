0c33b9c609f693195f3de57512f2db7d001b4fcf
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-04 14:38:58 +0600

    if else condition

---
+++ JsFoundation/part3/condition-challanges.js
@@ -0,0 +1,74 @@
+// cheack is a number is greater tha another number
+
+// let num1 = 2;
+// let num2 = 3;
+
+// if (num1 > num2) {
+//   console.log("number 1 is less than number 2");
+// } else {
+//   console.log("number 1 is not greater");
+// }
+
+// let a = 10;
+// let b = 5;
+
+// if (a > b) {
+//   console.log("a is greater");
+// } else if (a < b) {
+//   console.log("b is greater than a");
+// } else {
+//   console.log("both are equal");
+// }
+
+// let a = 5;
+// let b = 5;
+
+// if (a > b) {
+//     console.log('a is greater than b');
+// }else if (a < b) {
+//     console.log('b is greater than a');
+// }else{
+//     console.log('both are equal');
+// }
+
+// cheack if string equal to another string
+
+// let username = 'rifat'
+// let anotherUsername = 'rifat'
+
+// if (username === anotherUsername) {
+//     console.log('both are same username');
+// }else{
+//     console.log('the usrname is not same');
+// }
+
+// cheack if a varialble number or not
+
+// let a = '20'
+
+// if (typeof a === 'number') {
+//     console.log('they are number');
+// }else{
+//     console.log('they are not number');
+// }
+
+// cheack if a boolan number is true or flase
+
+// let isLoggedIn = true
+
+// if (isLoggedIn) {
+//     console.log('user is logged in');
+// }else{
+//     console.log('user is not logged in ');
+// }
+
+// cheack if an array is an empty arrray or not
+
+let a = ['20','25']
+
+if (a.length === 0) {
+    console.log('array is empty');
+    
+}else{
+    console.log('array is not empty');
+}
