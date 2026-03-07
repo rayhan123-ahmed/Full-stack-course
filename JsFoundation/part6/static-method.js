5e9e37d0b14e3bea2c03df22065ef1601d60446e
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-08 00:05:54 +0600

    Static method

---
+++ JsFoundation/part6/static-method.js
@@ -0,0 +1,10 @@
+// static method
+class Notification {
+    static send(){
+        return `sending tesxt......`
+    }
+}
+
+console.log(Notification.send());
+
+// so the work of static method is class can acces the function directly without help of object
