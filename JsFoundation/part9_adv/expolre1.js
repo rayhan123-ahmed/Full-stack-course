cbf6ad77261bc9d6ea813d8d4c51eb8d26465163
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-10 12:04:36 +0600

    asyncronous and event loop

---
+++ JsFoundation/part9_adv/expolre1.js
@@ -0,0 +1,12 @@
+console.log('Rayhan ahmed rifat');
+
+function sayHi() {
+    console.log('hey'); 
+}
+setTimeout(() => {
+    sayHi()
+}, 2000);
+
+for (let index = 0; index < 10; index++) { 
+    console.log(index); 
+}
