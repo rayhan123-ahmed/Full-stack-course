d4eb4bbda6708c3c295b0407a43ecc8fed414136
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-11 12:27:17 +0600

    import export file by a Modules

---
+++ JsFoundation/part9_adv/appM.js
@@ -0,0 +1,14 @@
+// Module import file
+
+
+// default import
+import subtract from "./mathOperationsM.js";
+
+// named funtion
+import { add, multiply } from "./mathOperationsM.js";
+
+console.log(subtract(2,5));
+console.log(add(5,8));
+console.log(multiply(5,8));
+
+
