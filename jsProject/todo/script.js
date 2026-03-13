0338ea9b987181cd0712608e38c4043ef9a908fd
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-13 13:37:13 +0600

    save task in localStorage

--- jsProject/todo/script.js
+++ jsProject/todo/script.js
@@ -1,18 +1,30 @@
-const todoInput =document.getElementById("todo-input");
-const addTAskBtn =document.getElementById("add-task-btn");
-const todoList=document.getElementById("todo-list");
+document.addEventListener('DOMContentLoaded',()=>{
+    const todoInput = document.getElementById("todo-input");
+    const addTAskBtn = document.getElementById("add-task-btn");
+    const todoList = document.getElementById("todo-list");
 
-let task = []
+    let tasks =JSON.parse(localStorage.getItem('task')) || [];
+     tasks.forEach((task => renderTask(task)))
 
-addTAskBtn.addEventListener('click',()=>{
-    const textTask = todoInput.value.trim()
-    if(textTask === '') return
-     newTask = {
+    addTAskBtn.addEventListener("click", () => {
+      const textTask = todoInput.value.trim();
+      if (textTask === "") return;
+      newTask = {
         id: Date.now(),
-        text:textTask,
-        isCompleted: false
+        text: textTask,
+        isCompleted: false,
+      };
+      tasks.push(newTask);
+      saveTask();
+      todoInput.value = "";
+      console.log(tasks);
+    });
+
+    function renderTask(task) {
+      console.log(task);
     }
-    task.push(newTask);
-    console.log(task);
-    
+
+    function saveTask() {
+      localStorage.setItem("task", JSON.stringify(tasks));
+    }
 })
