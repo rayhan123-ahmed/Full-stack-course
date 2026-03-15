724cea05c8734154fd5571e5363a69882c7e5ea6
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-15 17:40:20 +0600

    Add product list and cart setup

---
+++ jsProject/Ecommarce/script.js
@@ -0,0 +1,13 @@
+const productList = document.getElementById("product-list");
+const cartItems = document.getElementById("cart-items");
+const emptyCard = document.getElementById("empty-card");
+const cardTotal = document.getElementById("cart-total");
+const totalPrice = document.getElementById("total-price");
+const cheackoutBtn = document.getElementById("cheackout-btn");
+
+const products = [
+    {id:1, name:'product 1', price:29.99},
+    {id:2, name:'product 2', price:19.99},
+    {id:3, name:'product 3', price:9.99}
+]
+const cart = []
