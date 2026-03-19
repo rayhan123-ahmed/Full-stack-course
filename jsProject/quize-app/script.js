ed04730b6ab164294c3a3551ab7dc9e9377b01bd
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-03-19 15:40:40 +0600

    add some of the lgoic in quiz app and add some css

--- jsProject/quize-app/script.js
+++ jsProject/quize-app/script.js
@@ -31,3 +31,71 @@ const questions = [
     answer: "William Shakespeare",
   },
 ];
+
+// Create a empty veriable
+let currentQuestionIndex = 0
+let score = 0
+
+// add EventListener to the start button
+startBtn.addEventListener("click", startQuiz);
+// next button
+nextBtn.addEventListener('click',()=>{
+   currentQuestionIndex++
+   if (currentQuestionIndex < questions.length) {
+      showQuestion()
+   }else{
+      showResult()
+   }
+})
+// restart button
+restartBtn.addEventListener('click',()=>{
+    currentQuestionIndex = 0;
+    score = 0;
+    startQuiz()
+})
+
+// start quiz funtion
+function startQuiz() {
+  startBtn.classList.add("hidden");
+  resultContainer.classList.add('hidden');
+  qustionContainer.classList.remove('hidden');
+  showQuestion()
+}
+// create a function for Showing question on display 
+
+function showQuestion() {
+    nextBtn.classList.add('hidden');
+    qustionText.innerText = questions[currentQuestionIndex].question;
+    choicesList.innerHTML = ''; //clear previous choices
+    questions[currentQuestionIndex].choices.forEach(choice =>{
+        const li = document.createElement('li');
+        li.textContent = choice;
+        li.addEventListener('click',() => selectAnswer(choice,li));
+        choicesList.appendChild(li)
+    })
+}
+
+// create a funtion for selecting the correct answer
+function selectAnswer(choice,element) {
+  const correctAnswer = questions[currentQuestionIndex].answer;
+
+  // remove previous selection
+  document.querySelectorAll("#choices-list li").forEach((li) => {
+    li.classList.remove("selected");
+  });
+
+  // add selected style
+  element.classList.add("selected");
+
+  if (choice === correctAnswer) {
+    score++;
+  }
+  nextBtn.classList.remove("hidden");
+}
+
+// create a funtion for shwoing result
+function showResult() {
+   qustionContainer.classList.add('hidden');
+   resultContainer.classList.remove('hidden');
+   scoreDisplay.innerHTML = `${score} out of ${questions.length}`
+}
